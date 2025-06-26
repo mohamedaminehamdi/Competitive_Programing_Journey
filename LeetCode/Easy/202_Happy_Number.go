8f3ea1eac242ca38f5497cc46e78f1ba99615294
minyarbenmabrouk <minyarbenmabrouk@gmail.com>
2025-06-26 08:31:05 +0100

    Add 202_Happy_Number.go

---
+++ LeetCode/Easy/202_Happy_Number.go
@@ -0,0 +1,21 @@
+func isHappy(n int) bool {
+    // Helper function to compute sum of squares of digits
+    digitSumSquare := func(n int) int {
+        sum := 0
+        for n != 0 {
+            digit := n % 10
+            sum += digit * digit
+            n /= 10
+        }
+        return sum
+    }
+
+    seen := make(map[int]bool) // To detect cycles
+
+    for n != 1 {
+        if seen[n] {
+            return false // Loop detected
+        }
+        seen[n] = true
+        n = digitSumSquare(n)
+    }
